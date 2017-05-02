40bbe04ef9e554af97294a4cbff259a355469dd6
Nikolas Chaconas <nikolas_chaconas@umail.ucsb.edu>
2017-05-01 20:02:56 -0700

    first connect does make and git pull

--- algorithms/multi_thread_random/deploy.sh
+++ algorithms/multi_thread_random/deploy.sh
@@ -3,6 +3,7 @@
 
 ssh nikolas_chaconas@csil-01.cs.ucsb.edu 'bash -s' < 'csil_scripts/git_pull.sh' &
 
+sleep 5
 for i in {10..48}
 do
 	ssh nikolas_chaconas@csil-$i.cs.ucsb.edu 'bash -s' < 'csil_scripts/setup.sh' &
