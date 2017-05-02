37c513a417b2c7589954aa125797a8d39443ea3b
Nikolas Chaconas <nikolas_chaconas@umail.ucsb.edu>
2017-05-01 19:55:37 -0700

    mass upload/terminate to csil using deploy and temrinate scripts

---
+++ algorithms/multi_thread_random/csil_scripts/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd ~/Ramseys-bane/algorithms/multi_thread_random/
+make
+make run
+echo "exiting..."
---
+++ algorithms/multi_thread_random/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+ssh nikolas_chaconas@csil-01.cs.ucsb.edu 'bash -s' < 'csil_scripts/git_pull.sh' &
+
+for i in {10..48}
+do
+	ssh nikolas_chaconas@csil-$i.cs.ucsb.edu 'bash -s' < 'csil_scripts/setup.sh' &
+	echo "Sent to CSIL-$i"
+done
