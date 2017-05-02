ea6982f1b6f418e921bf92faa9f2b925a3117e14
Nikolas Chaconas <nikolas_chaconas@umail.ucsb.edu>
2017-05-01 22:19:13 -0700

    updated deploy scripts to deploy to CSIL and HONEA labs

--- algorithms/multi_thread_random/deploy.sh
+++ algorithms/multi_thread_random/deploy.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
 
-ssh nikolas_chaconas@csil-01.cs.ucsb.edu 'bash -s' < 'csil_scripts/git_pull.sh' &
+ssh -o "StrictHostKeyChecking no" nikolas_chaconas@csil-01.cs.ucsb.edu 'bash -s' < 'csil_scripts/git_pull.sh' &
 
-sleep 5
-for i in {10..48}
+sleep 10
+for i in {1..48}
 do
-	ssh nikolas_chaconas@csil-$i.cs.ucsb.edu 'bash -s' < 'csil_scripts/setup.sh' &
-	echo "Sent to CSIL-$i"
+	if [ "$i" -lt 10 ]
+	then
+		j="0$i"
+	else
+		j="$i"
+	fi
+	if [ "$j" -lt 29 ]
+	then
+		ssh -o "StrictHostKeyChecking no" nikolas_chaconas@linux$j.engr.ucsb.edu 'bash -s' < 'csil_scripts/setup.sh' &
+		echo "Sent to linux$j.engr.ucsb.edu"
+	fi
+	ssh nikolas_chaconas@csil-$j.cs.ucsb.edu 'bash -s' < 'csil_scripts/setup.sh' &
+	echo "Sent to csil-$j.cs.ucsb.edu"
 done
