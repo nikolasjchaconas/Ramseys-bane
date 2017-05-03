f5b419aca5b23cb8d42df08eddad71c77ec8ec26
Nikolas Chaconas <nikolas_chaconas@umail.ucsb.edu>
2017-05-03 16:41:59 -0700

    logs are ignored by github

--- algorithms/main.c
+++ algorithms/main.c
@@ -29,10 +29,10 @@ int main() {
 	//set whether you want counter examples written to disk
 	//(condor does not allow this)
 	// set to 1 or 0
-	WRITE_TO_FILE = 1;
+	WRITE_TO_FILE = 0;
 
 	// set thread type
 	// can be either SINGLE_THREAD or MULTI_THREAD
-	setThreads(MULTI_THREAD);
+	setThreads(SINGLE_THREAD);
 	return 0;
 }
