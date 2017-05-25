e5ba1b084bec08fc45fc650d5817b106827db0e0
Nikolas Chaconas <nikolas_chaconas@umail.ucsb.edu>
2017-05-25 10:53:09 -0700

    updated scripts to use key in .ssh

--- distributed_sys_flip/scripts/deploy_euca.sh
+++ distributed_sys_flip/scripts/deploy_euca.sh
@@ -2,70 +2,70 @@
 
 echo "Sending to Small Eucalyptus Hosts"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem root@128.111.84.222 'bash -s 1' < './deploy.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem root@128.111.84.222 'bash -s 1' < './deploy.sh' &
 echo "Sent to root@128.111.84.222"
-printf "Check it out here: \nssh -i coordinator_key.pem root@128.111.84.222\n\n"
+printf "Check it out here: \nssh -i ~/.ssh/coordinator_key.pem root@128.111.84.222\n\n"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem root@128.111.84.192 'bash -s 1' < './deploy.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem root@128.111.84.192 'bash -s 1' < './deploy.sh' &
 echo "Sent to root@128.111.84.192"
-printf "Check it out here: \nssh -i coordinator_key.pem root@128.111.84.192\n\n"
+printf "Check it out here: \nssh -i ~/.ssh/coordinator_key.pem root@128.111.84.192\n\n"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem root@128.111.84.218 'bash -s 1' < './deploy.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem root@128.111.84.218 'bash -s 1' < './deploy.sh' &
 echo "Sent to root@128.111.84.218"
-printf "Check it out here: \nssh -i coordinator_key.pem root@128.111.84.218\n\n"
+printf "Check it out here: \nssh -i ~/.ssh/coordinator_key.pem root@128.111.84.218\n\n"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem root@128.111.84.203 'bash -s 1' < './deploy.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem root@128.111.84.203 'bash -s 1' < './deploy.sh' &
 echo "Sent to root@128.111.84.203"
-printf "Check it out here: \nssh -i coordinator_key.pem root@128.111.84.203\n\n"
+printf "Check it out here: \nssh -i ~/.ssh/coordinator_key.pem root@128.111.84.203\n\n"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem root@128.111.84.231 'bash -s 1' < './deploy.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem root@128.111.84.231 'bash -s 1' < './deploy.sh' &
 echo "Sent to root@128.111.84.231"
-printf "Check it out here: \nssh -i coordinator_key.pem root@128.111.84.231\n\n"
+printf "Check it out here: \nssh -i ~/.ssh/coordinator_key.pem root@128.111.84.231\n\n"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem root@128.111.84.189 'bash -s 1' < './deploy.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem root@128.111.84.189 'bash -s 1' < './deploy.sh' &
 echo "Sent to root@128.111.84.189"
-printf "Check it out here: \nssh -i coordinator_key.pem root@128.111.84.189\n\n"
+printf "Check it out here: \nssh -i ~/.ssh/coordinator_key.pem root@128.111.84.189\n\n"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem root@128.111.84.244 'bash -s 1' < './deploy.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem root@128.111.84.244 'bash -s 1' < './deploy.sh' &
 echo "Sent to root@128.111.84.244"
-printf "Check it out here: \nssh -i coordinator_key.pem root@128.111.84.244\n\n"
+printf "Check it out here: \nssh -i ~/.ssh/coordinator_key.pem root@128.111.84.244\n\n"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem root@128.111.84.191 'bash -s 1' < './deploy.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem root@128.111.84.191 'bash -s 1' < './deploy.sh' &
 echo "Sent to root@128.111.84.191"
-printf "Check it out here: \nssh -i coordinator_key.pem root@128.111.84.191\n\n"
+printf "Check it out here: \nssh -i ~/.ssh/coordinator_key.pem root@128.111.84.191\n\n"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem root@128.111.84.172 'bash -s 1' < './deploy.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem root@128.111.84.172 'bash -s 1' < './deploy.sh' &
 echo "Sent to root@128.111.84.172"
-printf "Check it out here: \nssh -i coordinator_key.pem root@128.111.84.172\n\n"
+printf "Check it out here: \nssh -i ~/.ssh/coordinator_key.pem root@128.111.84.172\n\n"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem root@128.111.84.251 'bash -s 1' < './deploy.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem root@128.111.84.251 'bash -s 1' < './deploy.sh' &
 echo "Sent to root@128.111.84.251"
-printf "Check it out here: \nssh -i coordinator_key.pem root@128.111.84.251\n\n"
+printf "Check it out here: \nssh -i ~/.ssh/coordinator_key.pem root@128.111.84.251\n\n"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem  root@128.111.84.219 'bash -s 1' < './deploy.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem  root@128.111.84.219 'bash -s 1' < './deploy.sh' &
 echo "Sent to  root@128.111.84.219"
-printf "Check it out here: \nssh -i coordinator_key.pem root@128.111.84.219\n\n"
+printf "Check it out here: \nssh -i ~/.ssh/coordinator_key.pem root@128.111.84.219\n\n"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem root@128.111.84.187 'bash -s 1' < './deploy.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem root@128.111.84.187 'bash -s 1' < './deploy.sh' &
 echo "Sent to root@128.111.84.187"
-printf "Check it out here: \nssh -i coordinator_key.pem root@128.111.84.187\n\n"
+printf "Check it out here: \nssh -i ~/.ssh/coordinator_key.pem root@128.111.84.187\n\n"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem root@128.111.84.170 'bash -s 1' < './deploy.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem root@128.111.84.170 'bash -s 1' < './deploy.sh' &
 echo "Sent to root@128.111.84.170"
-printf "Check it out here: \nssh -i coordinator_key.pem root@128.111.84.170\n\n"
+printf "Check it out here: \nssh -i ~/.ssh/coordinator_key.pem root@128.111.84.170\n\n"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem root@128.111.84.253 'bash -s 1' < './deploy.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem root@128.111.84.253 'bash -s 1' < './deploy.sh' &
 echo "Sent to root@128.111.84.253"
-printf "Check it out here: \nssh -i coordinator_key.pem root@128.111.84.253\n\n"
+printf "Check it out here: \nssh -i ~/.ssh/coordinator_key.pem root@128.111.84.253\n\n"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem root@128.111.84.230 'bash -s 1' < './deploy.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem root@128.111.84.230 'bash -s 1' < './deploy.sh' &
 echo "Sent to root@128.111.84.230"
-printf "Check it out here: \nssh -i coordinator_key.pem root@128.111.84.230\n\n"
+printf "Check it out here: \nssh -i ~/.ssh/coordinator_key.pem root@128.111.84.230\n\n"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem root@128.111.84.224 'bash -s 1' < './deploy.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem root@128.111.84.224 'bash -s 1' < './deploy.sh' &
 echo "Sent to root@128.111.84.224"
-printf "Check it out here: \nssh -i coordinator_key.pem root@128.111.84.224\n\n"
+printf "Check it out here: \nssh -i ~/.ssh/coordinator_key.pem root@128.111.84.224\n\n"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem root@128.111.84.193 'bash -s 1' < './deploy.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem root@128.111.84.193 'bash -s 1' < './deploy.sh' &
 echo "Sent to root@128.111.84.193"
-printf "Check it out here: \nssh -i coordinator_key.pem root@128.111.84.193\n\n"
+printf "Check it out here: \nssh -i ~/.ssh/coordinator_key.pem root@128.111.84.193\n\n"
--- distributed_sys_flip/scripts/terminate_euca.sh
+++ distributed_sys_flip/scripts/terminate_euca.sh
@@ -1,52 +1,52 @@
 #!/bin/bash
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem root@128.111.84.222 'bash -s' < './terminate.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem root@128.111.84.222 'bash -s' < './terminate.sh' &
 echo "Sent to root@128.111.84.222"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem root@128.111.84.192 'bash -s' < './terminate.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem root@128.111.84.192 'bash -s' < './terminate.sh' &
 echo "Sent to root@128.111.84.192"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem root@128.111.84.218 'bash -s' < './terminate.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem root@128.111.84.218 'bash -s' < './terminate.sh' &
 echo "Sent to root@128.111.84.218"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem root@128.111.84.203 'bash -s' < './terminate.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem root@128.111.84.203 'bash -s' < './terminate.sh' &
 echo "Sent to root@128.111.84.203"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem root@128.111.84.231 'bash -s' < './terminate.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem root@128.111.84.231 'bash -s' < './terminate.sh' &
 echo "Sent to root@128.111.84.231"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem root@128.111.84.189 'bash -s' < './terminate.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem root@128.111.84.189 'bash -s' < './terminate.sh' &
 echo "Sent to root@128.111.84.189"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem root@128.111.84.244 'bash -s' < './terminate.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem root@128.111.84.244 'bash -s' < './terminate.sh' &
 echo "Sent to root@128.111.84.244"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem root@128.111.84.191 'bash -s' < './terminate.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem root@128.111.84.191 'bash -s' < './terminate.sh' &
 echo "Sent to root@128.111.84.191"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem root@128.111.84.172 'bash -s' < './terminate.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem root@128.111.84.172 'bash -s' < './terminate.sh' &
 echo "Sent to root@128.111.84.172"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem root@128.111.84.251 'bash -s' < './terminate.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem root@128.111.84.251 'bash -s' < './terminate.sh' &
 echo "Sent to root@128.111.84.251"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem  root@128.111.84.219 'bash -s' < './terminate.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem  root@128.111.84.219 'bash -s' < './terminate.sh' &
 echo "Sent to root@128.111.84.219"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem root@128.111.84.187 'bash -s' < './terminate.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem root@128.111.84.187 'bash -s' < './terminate.sh' &
 echo "Sent to root@128.111.84.187"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem root@128.111.84.170 'bash -s' < './terminate.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem root@128.111.84.170 'bash -s' < './terminate.sh' &
 echo "Sent to root@128.111.84.170"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem root@128.111.84.253 'bash -s' < './terminate.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem root@128.111.84.253 'bash -s' < './terminate.sh' &
 echo "Sent to root@128.111.84.253"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem root@128.111.84.230 'bash -s' < './terminate.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem root@128.111.84.230 'bash -s' < './terminate.sh' &
 echo "Sent to root@128.111.84.230"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem root@128.111.84.224 'bash -s' < './terminate.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem root@128.111.84.224 'bash -s' < './terminate.sh' &
 echo "Sent to root@128.111.84.224"
 
-ssh -o "StrictHostKeyChecking no" -i coordinator_key.pem root@128.111.84.193 'bash -s' < './terminate.sh' &
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/coordinator_key.pem root@128.111.84.193 'bash -s' < './terminate.sh' &
 echo "Sent to root@128.111.84.193"
