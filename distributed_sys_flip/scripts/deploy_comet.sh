953ef8c897b6a081b4619999b6aef567a4a018d2
Nikolas Chaconas <nikolas_chaconas@umail.ucsb.edu>
2017-06-02 16:49:07 -0700

    fix compilation for comet

--- distributed_sys_flip/scripts/deploy_comet.sh
+++ distributed_sys_flip/scripts/deploy_comet.sh
@@ -3,7 +3,7 @@
 cd ~/Ramseys-bane/distributed_sys_flip
 git stash
 git pull origin master
-mpicc thread.c clique-count.c greedyGraphPermute.c distributed_flip.c client_protocol/client.c matrix.c getcpuclockspeed.c -pthread -lm
+mpicc thread.c clique-count.c greedyGraphPermute.c distributed_flip.c client_protocol/client.c matrix.c getcpuclockspeed.c cliquer.c graph.c reorder.c -pthread -lm
 
 sbatch jobscriptfile
 
