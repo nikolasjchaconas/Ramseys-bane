a27856a87db4244a7fa0c6a18e84395340692e97
Nikolas Chaconas <nikolas_chaconas@umail.ucsb.edu>
2017-06-02 21:37:30 -0700

    better deployment cstl

---
+++ distributed_sys_flip/scripts/deploy_s.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ "$#" -ne 1 ]; then
+    printf "Usage: ./deploy.sh NUM_THREADS\nPlease pass number of threads as first arguments\n"
+    exit
+fi
+cd ~/Ramseys-bane/distributed_sys_flip/scripts
+./terminate.sh
+nohup ./setup_csil.sh $1 > /dev/null 2>&1 &
