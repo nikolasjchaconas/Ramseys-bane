456493015188be57a47e584f842ff9e2930ad62e
Nikolas Chaconas <nikolas_chaconas@umail.ucsb.edu>
2017-06-06 05:06:09 -0700

    forgot heuristic search header file

---
+++ distributed_sys_flip/heuristicsearch.h
@@ -0,0 +1,9 @@
+#ifndef HEURISTICSEARCH_H
+#define HEURISTICSEARCH_H
+
+#include <math.h>
+#include "matrix.h"
+
+int replaceMe(int* g, int nodeCount, int cliqueCount, client_struct* client_info) ;
+
+#endif
