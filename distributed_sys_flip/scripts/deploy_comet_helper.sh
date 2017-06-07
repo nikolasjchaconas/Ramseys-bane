2fbace3f0af00a6d53c840162a5efc1b1abcb5d7
Nikolas Chaconas <nikolas_chaconas@umail.ucsb.edu>
2017-06-06 17:54:05 -0700

    updated comet deploy:

--- distributed_sys_flip/scripts/deploy_comet_helper.sh
+++ distributed_sys_flip/scripts/deploy_comet_helper.sh
@@ -3,7 +3,7 @@
 cd ~/Ramseys-bane/distributed_sys_flip
 git fetch --all
 git reset --hard origin/master
-mpicc thread.c clique-count.c greedyGraphPermute.c distributed_flip.c client_protocol/client.c matrix.c getcpuclockspeed.c cliquer.c graph.c reorder.c -pthread -lm
+mpicc thread.c clique-count.c greedyGraphPermute.c distributed_flip.c client_protocol/client.c matrix.c getcpuclockspeed.c cliquer.c graph.c reorder.c heuristicsearch.c -pthread -lm
 
 sbatch jobscriptfile
 
